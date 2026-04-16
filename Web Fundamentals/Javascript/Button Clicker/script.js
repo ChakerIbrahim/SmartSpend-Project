d8f7774b578f4babc98ecb099ce3ca9ae1f93818
Jalil <wasaya.jalil@gmail.com>
2026-04-16 15:23:38 +0300

    grid

--- Web Fundamentals/Javascript/Button Clicker/script.js
+++ Web Fundamentals/Javascript/Button Clicker/script.js
@@ -2,6 +2,10 @@ function removeBtn(element){
     element.remove()
 }
 
+function getAlert(){
+    alert('Ninja was liked')
+}
+
 function loginOut(element){
     if (element.innerText === "login"){
         element.innerText = "logout";
