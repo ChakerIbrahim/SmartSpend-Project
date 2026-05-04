3eb88e344546a27d2c189dcdbcfc802332b64233
Jalil <wasaya.jalil@gmail.com>
2026-05-04 13:35:08 +0300

    HelloWorld

---
+++ python_stack/_python/python_fundamentals/Hello World/HelloWorld.py
@@ -0,0 +1,17 @@
+print("Hello World")
+
+name = "Jalil"
+print("My name is",name)
+print("My name is " + name )
+
+name = 42
+print("Hello",name)
+# print("Hello" + name)
+
+fav_food1 = "Sushi"
+fav_food2 = "Pizza"
+print("I love to eat {} and {}".format(fav_food2,fav_food1))
+print(f"I love to eat {fav_food1} and {fav_food2}.")
+
+
+
