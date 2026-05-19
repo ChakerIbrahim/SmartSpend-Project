84dbe4df334f0519a7983984865dd7eb14165eaf
Jalil <wasaya.jalil@gmail.com>
2026-05-19 21:17:46 +0300

    Survey

--- python_stack/django/multi_apps/multi_apps/urls.py
+++ python_stack/django/multi_apps/multi_apps/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for time_project project.
+URL configuration for multi_apps project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -15,9 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import path
 from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('time_display.urls')),
+    path('',include('users.urls')),
+    path('blogs/',include('blogs.urls')),
+    path('surveys/',include('surveys.urls')),
 ]
